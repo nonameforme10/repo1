bab8c7a0864129d2daceff477d8a0f5ab5966b1a
Hunterstar <kyuter789@gmail.com>
2026-03-22 20:12:35 +0500

    fixed

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const IS_SERVICE_WORKER_CONTEXT =
   self instanceof ServiceWorkerGlobalScope;
 
 if (IS_SERVICE_WORKER_CONTEXT) {
-  const VERSION = "v12.1";
+  const VERSION = "v1.1.1.1.1";
   const CORE_CACHE = `eduventure-core-${VERSION}`;
   const RUNTIME_CACHE = `eduventure-runtime-${VERSION}`;
   const MAX_RUNTIME_ENTRIES = 120;
